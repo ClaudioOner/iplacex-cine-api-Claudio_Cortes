faa6cb0171c3e36e2e27ff330d99272ed5a008f5
ClaudioOner <claudio.cortes@marval.cl>
2024-09-22 20:55:24 -0300

    Update db.js

--- src/common/db.js
+++ src/common/db.js
@@ -1,7 +1,7 @@
 import { MongoClient, ServerApiVersion } from 'mongodb'
 
 
-const uri = 'mongodb+srv://ev3_express:MILfoxQWZdQJZ7UJ@cluster-express.ljt2z.mongodb.net/?retryWrites=true&w=majority&appName=cluster-express'
+
 
 
 const client = new MongoClient(uri, {
