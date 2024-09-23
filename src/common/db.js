c70413231c0afc0a7ed4a196254655341fa12873
ClaudioOner <claudio.cortes@marval.cl>
2024-09-22 21:09:41 -0300

    Update db.js

--- src/common/db.js
+++ src/common/db.js
@@ -1,7 +1,6 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import { MongoClient, ServerApiVersion } from 'mongodb'
 
-
-const uri = 'mongodb+srv://ev3_express:MILfoxQWZdQJZ7UJ@cluster-express.ljt2z.mongodb.net/cine-db?retryWrites=true&w=majority&tls=true';
+const uri = 'mongodb+srv://ev3_express:MILfoxQWZdQJZ7UJ@cluster-express.ljt2z.mongodb.net/?retryWrites=true&w=majority&appName=cluster-express'
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -9,6 +8,6 @@ const client = new MongoClient(uri, {
         strict: true,
         deprecationErrors: true
     }
-});
+})
 
-export default client;
+export default client
